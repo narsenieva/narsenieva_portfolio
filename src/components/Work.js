9276dfc52df29a9f73b1c9caa22627e0b178751f
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2022-04-28 21:56:48 -0400

    Initial commit and pagination

---
+++ src/components/Work.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Work = () => {
+  return (
+    <div>
+      <h1>work</h1>
+    </div>
+  )
+}
+export default Work
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Home from './Home';
+import Work from './Work';
+import About from './About';
+import Contact from './Contact';
+export { Home, Work, About, Contact};
