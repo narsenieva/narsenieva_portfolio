ea1d6c05009574f32c48c924c0353276bfc629c6
Nataliia Arsenieva <nataly.arsenieva@gmail.com>
2022-04-28 22:40:37 -0400

    Loading placeholder added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home, About, Work, Contact} from "./components";
+import { Home, About, Work, Contact, Loader} from "./components";
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(()=>{ 
+      setLoading(false);
+    }, 4000);
+  }, []);
   return (
-    <Router>
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route path="work" element={<Work/>} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
-    </Routes>
-  </Router>
+    <div>
+    {loading ?
+      <Loader></Loader>
+      : 
+        <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="work" element={<Work/>} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+        </Router>
+      }
+    </div>
   );
 }
 
